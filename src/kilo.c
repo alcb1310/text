1c37f2567b7525b4186ed3f77814ee9331f53d40
Andres Court <andres@andrescourt.com>
2025-07-10 17:48:37 -0500

    feat: display welcome screen

--- src/kilo.c
+++ src/kilo.c
@@ -1,7 +1,6 @@
 /*** includes ***/
 
 #include <asm-generic/ioctls.h>
-#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +11,7 @@
 
 /*** defines ***/
 
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -227,7 +227,30 @@ void abFree(struct abuf *ab) { free(ab->b); }
  */
 void editorDrawRows(struct abuf *ab) {
   for (int y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+                                "Kilo Editor -- Version %s", KILO_VERSION);
+
+      if (welcomelen > E.screencols) {
+        welcomelen = E.screencols;
+      }
+
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        // Add some padding
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+
+      while (padding--) {
+        abAppend(ab, " ", 1);
+      }
+
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
     abAppend(ab, "\x1b[K", 3); // clear line
     if (y < E.screenrows - 1) {
