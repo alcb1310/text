3535d85401444a7df6ba7093a472285c8a0c521d
Andres Court <andres@andrescourt.com>
2025-07-10 16:45:19 -0500

    feat: draw tildes to screen

--- src/kilo.c
+++ src/kilo.c
@@ -130,12 +130,25 @@ char editorReadKey() {
 
 /*** output ***/
 
+/*
+ * Draws information to the screen
+ */
+void editorDrawRows() {
+  for (int y = 0; y < 25; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
 /***
  * Refreshes the screen
  * https://vt100.net/docs/vt100-ug/chapter3.html#ED
  */
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
