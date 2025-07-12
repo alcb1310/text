9fbaba4a81cb1e4b6307b6d279c9bb7038d28c1a
Andres Court <andres@andrescourt.com>
2025-07-12 14:35:20 -0500

    feat: restore cursor position when cancel search

--- src/kilo.c
+++ src/kilo.c
@@ -658,9 +658,20 @@ void editorFindCallback(char *query, int key) {
  * Prompts and find that prompt within the text
  */
 void editorFind() {
+  // save the current cursor position
+  int saved_cx = E.cx;
+  int saved_cy = E.cy;
+  int saved_coloff = E.coloff;
+  int saved_rowoff = E.rowoff;
+
   char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
   if (query) {
     free(query);
+  } else {
+    E.cx = saved_cx;
+    E.cy = saved_cy;
+    E.coloff = saved_coloff;
+    E.rowoff = saved_rowoff;
   }
 }
 
