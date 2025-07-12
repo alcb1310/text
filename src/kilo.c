5f4f8e652ff868b8d90ffff9dda060ba2b196773
Andres Court <andres@andrescourt.com>
2025-07-12 09:35:49 -0500

    feat: enter key behavior

--- src/kilo.c
+++ src/kilo.c
@@ -360,10 +360,14 @@ void editorUpdateRow(erow *row) {
 /***
  * Appends a new row to the end of the row array
  */
-void editorAppendRow(char *s, size_t len) {
-  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+  if (at < 0 || at > E.numrows) {
+    return;
+  }
 
-  int at = E.numrows;
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -465,11 +469,30 @@ void editorRowDelChar(erow *row, int at) {
  */
 void editorInsertChar(int c) {
   if (E.cy == E.numrows) {
-    editorAppendRow("", 0);
+    editorInsertRow(E.numrows, "", 0);
   }
 
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+/***
+ * Inserts a new line
+ */
+void editorInsertNewLine() {
+  if (E.cx == 0) {
+    editorInsertRow(E.cy, "", 0);
+  } else {
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+
+  E.cy++;
+  E.cx = 0;
 }
 
 /***
@@ -544,7 +567,7 @@ void editorOpen(char *filename) {
       linelen--;
     }
 
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
 
   free(line);
@@ -849,8 +872,8 @@ void editorNormalProcessKeypress(int c) {
     exit(EXIT_SUCCESS);
     break;
 
-  case DEL_KEY:
-    // TODO: implement
+  case '\r':
+    editorMoveCursor(ARROW_DOWN);
     break;
 
   case CTRL_KEY('s'):
@@ -934,7 +957,7 @@ void editorInsertProcessKeypress(int c) {
     break;
 
   case '\r':
-    // TODO: implement
+    editorInsertNewLine();
     break;
 
   case CTRL_KEY('l'):
