6354a376f906b05630ec1f01eb0c09328262a381
Andres Court <andres@andrescourt.com>
2025-07-13 12:43:51 -0500

    refactor(split): move the input functions to its own file

---
+++ src/input.h
@@ -0,0 +1,13 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include "typedefs.h"
+
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
+void editorMoveCursor(int key);
+void editorNormalProcessKeypress(int c);
+void editorInsertProcessKeypress(int c);
+void editorProcessKeypress();
+void editorSetStatusMessage(const char *fmt, ...);
+
+#endif // !INPUT_H
